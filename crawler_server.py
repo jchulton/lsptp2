b60c4ca7df17588e7a604ccfed6fd01f0c6bf87e
Maxfield Herman <leawoodmax@yahoo.com>
2019-11-20 17:00:43 -0500

    added more notes and created the JSON object to be sent to DDS

--- crawler_server.py
+++ crawler_server.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from flask import request
-import soup.py
+import json
+import soup.py as crawler
+import time
 app = Flask(__name__)
+
+LA_url = ""
+DDS_url = ""
 
 # test to see if working properly
 @app.route('/')
@@ -12,16 +17,39 @@ def hello_world():
 def crawl_link():
     link = request.args.get('url')
     # operate on url
-    
+    crawler_output = crawler.crawl(link)
     # send output to link analysis
-    
+    request.post(LA_url, param={'urls': outgoing_links})
     # format JSON object for DDS
-    
+    DDS_output = dict()
+    DDS_output["link"] = link
+    DDS_output["status"] = crawler_output["status"] # will crawler return this???
+    DDS_output["text"] = "" # are we not giving the raw html anymore???
+    DDS_output["crawled_links"] = crawler_output[outgoing_links]
+    DDS_output["last_updated_date"] = time.ctime()
     # send JSON object to DDS
-
+    request.post(LA_url, param=DDS_output)
 
 
 """
+NEED TO DO:
+send ACK to LA when we get input
+wait for ACK from DDS, and decide what to do if we don't recieve one in x time
+get info on how to connect with LA and DDS servers
+refine when/how recrawling will take place
+format with codeing style specifications
+
+
+JSON format:
+'{ 
+"link":"https://www.cs.rpi.edu/~goldsd/index.php", 
+"status":200, 
+"text":"I am all the text on the page (no I’m not but putting all the text would make this very messy)",
+"crawled_links": [], 
+"last_updated_date":""
+}'
+
+
 Document Data Store:
 PUSHING
     DELETE request:
