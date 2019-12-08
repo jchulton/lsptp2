b7f5426253c2f5b444ca2711ec98206f89547f39
Maxfield Herman <leawoodmax@yahoo.com>
2019-12-08 14:57:57 -0500

    made changes and added comments to main server

--- crawler_server.py
+++ crawler_server.py
@@ -7,7 +7,6 @@ import importlib
 from soup import crawl
 app = Flask(__name__)
 
-
 ### API Calls
 
 """
@@ -24,13 +23,16 @@ Output: Sends the link to a queue of urls which will be processed.
         Also sends an acknowledgment to LA
 Effects: queues a new link to q_links
 """
-@app.route('/crawl')
+@app.route('/crawl', methods=['POST'])
 def crawl_link():
+    global q_links
     print("Got LA data")
-    link = request.args.get('url')
-    q.put(link)
+    link = request.args
+    print(link)
+    q_links.put(link)
     # send output to link analysis
-    request.post(LA_url, param='/ack')
+    #start_main()
+    return 'ack'
 
 
 """
@@ -55,10 +57,10 @@ DDS_url = ""
 # queues used to store 
 q_links = queue.Queue()
 q_active = queue.Queue()
-
-# only allow at most 10 links to be processed at a time
 available = 0
 limit = 10
+
+text_file = open("output.txt", "w")
 
 
 ### Methods
@@ -68,14 +70,19 @@ input: none
 output: handles processing new links put into q_links then sends the data to LA and DDS
 """
 def start_main():
-    print("Operating on links")
+
+    # only allow at most 10 links to be processed at a time
+    global available, limit, q_links, q_active, DDS_url, LA_url, need_ack
+
+    text_file.write("Operating on links")
     
     # this should run forever as the server should never stop
     while True:
-        
+        text_file.write("proceeding")
+
         # get recrawl links from DDS
         if time.time() == 0:
-            print("Re Crawling links")
+            text_file.write("Re Crawling links")
             recrawls = request.get(DDS_url, param="/recrawl")
             for link in recrawls:
                 q_links.put(link)
@@ -83,7 +90,7 @@ def start_main():
         
         # if a new process space is available, start a new process
         if available < limit and not q_links.empty():
-            print("Crawling Link")
+            text_file.write("Crawling Link")
             json = dict()
             
             # create new process with a reference to a json object (dicitionary)
@@ -94,9 +101,10 @@ def start_main():
         
         # if a process is running, join it and send the output to LA and DDS
         if not q_active.empty():
-            print("Joining Link")
+            text_file.write("Joining Link")
             json, pro = q_active.get(True)
             pro.join()
+            text_file.write(json)
             
             # send output to link analysis
             request.post(LA_url, param={'urls': json['out_links']})
@@ -116,8 +124,8 @@ def start_main():
 Description: starts main loop
 """
 if __name__ == "__main__":
-    print("Server starting")
-    app.run(host='localhost', port=80)
+    text_file.write("Server starting")
+    app.run(host='localhost', port=2500)
     start_main()
             
 
