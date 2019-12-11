0b250d78e8a7331bb76cba46e2614cf13169f022
Maxfield Herman <leawoodmax@yahoo.com>
2019-12-11 17:29:55 -0500

    Rewrote all of server due to issues of server api blocking main code.

--- crawler_server.py
+++ crawler_server.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, current_app
-from flask_script import Manager
 from multiprocessing import Process
 import json
 import socket
@@ -9,24 +8,8 @@ import requests
 import importlib
 from soup import crawl
 app = Flask(__name__)
-manager = Manager(app)
 
 ### API Calls
-
-@app.before_first_request
-def activate_job():
-    #main = Process(target=start_main)
-    #main.start()
-    #main.join()
-    return
-
-"""
-Test to see if server is active and responding
-"""
-@app.route('/helloworld')
-def helloworld():
-    return "helloworld"
-
 """
 Description: Takes a /crawl request from L.A. and queues a new link
 Input: A crawl request with a single link (string) as a parameter
@@ -49,6 +32,7 @@ def crawl_link():
 Description: Recognize if an acknowledgement was recieved
 Input: An /ack request
 Effects: tells the server another componenet has recieved our data
+Modifies: nothing
 """
 @app.route('/ack')
 def acknowledgement():
@@ -56,7 +40,6 @@ def acknowledgement():
 
 
 ### Global Variables
-
 # Boolean variable showing whether an acknowledgement is needed
 need_ack = False
 
@@ -64,43 +47,41 @@ need_ack = False
 LA_url = "http://lspt-link2.cs.rpi.edu"
 DDS_url = ""
 
-# queues used to store 
-#q_links = []
-#active = []
-#completed_processes = []
-#available = 0
-#limit = 10
-
-
 ### Methods
 """
-Overview:
-Input:
-Output:
+Overview: Using the crawling algorithm, process the link and scrape the data from it
+Input: string - link, dictionary - json object
+Output: the reference object 'json_object' will store all data gathered during the crawling into said object
+Modifies: nothing
 """
 def scrape_link(link, json_object):
-    
+    # call the crawl algorithm on the given link
     json_object = crawl(link, json_object)
-    print("scraping output", json)
 
 """
-Overview:
-Input:
-Output:
+Overview: Send the gathered JSON object to DDS, and portions of it to LA
+Input: dictionary - json_object holding all pertinent information
+Output: None
+Effects: Sends data to both DDS and LA for them to store and manipulate, respectively 
 """
 def send_link_info(json_object):
 
     # send output to link analysis
     headers = {'X-API-TOKEN': 'return_links'}
     payload = {'link': json_object['inner-link'], 'outgoing': json_object["outbound-links"]}
-    #r = requests.post(LA_url, data=payload, headers=headers)
+    r1 = requests.post(LA_url, data=payload, headers=headers)
     
     # send JSON object to DDS
     headers = {'X-API-TOKEN': 'crawling_links'}
-    #r = requests.post(DDS_url, data=json_object, headers=headers)
 
-    return 1
+    # depending on what is gotten from this request, send a put/post request with the JSON object to DDS
+    if(requests.get(DDS_url, data={'link': json_object['inner-link']}, headers={'X-API-TOKEN': 'link'})):
+        r2 = requests.put(DDS_url, data=json_object, headers=headers)
+    else:
+        r2 = requests.post(DDS_url, data=json_object, headers=headers)
 
+
+# Starts the server
 if __name__ == "__main__":
     print("Server starting")
     app.run(host='localhost', port=2502)
